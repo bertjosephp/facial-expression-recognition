07a1633b9f62783001d815c123c2db25a3a9cb97
Bert Joseph Prestoza <87596273+bertjosephp@users.noreply.github.com>
2024-05-31 22:03:09 -0700

    Add temporary Net.py with model accuracy at 60%

---
+++ Net.py
@@ -0,0 +1,63 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=32)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_features=32)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.drop1 = nn.Dropout(0.25)
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(num_features=64)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(num_features=64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.drop2 = nn.Dropout(0.25)
+
+        self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(num_features=128)
+        self.conv6 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.bn6 = nn.BatchNorm2d(num_features=128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.drop3 = nn.Dropout(0.25)
+
+        self.conv7 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.bn7 = nn.BatchNorm2d(num_features=256)
+        self.conv8 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.bn8 = nn.BatchNorm2d(num_features=256)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.drop4 = nn.Dropout(0.25)
+
+        self.fc1 = nn.Linear(256 * 3 * 3, 256)
+        self.drop5 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 7)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.drop1(self.pool1(x))
+        
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.drop2(self.pool2(x))
+        
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.drop3(self.pool3(x))
+        
+        x = F.relu(self.bn7(self.conv7(x)))
+        x = F.relu(self.bn8(self.conv8(x)))
+        x = self.drop4(self.pool4(x))
+        
+        x = x.view(-1, 256 * 3 * 3)
+        x = F.relu(self.fc1(x))
+        x = self.drop5(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
