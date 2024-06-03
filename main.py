8dd42faa8c53d29203f1364165384ca09177ba1b
Bert Joseph Prestoza <87596273+bertjosephp@users.noreply.github.com>
2024-06-03 14:04:30 -0700

    Remove comment

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from FaceDetector import FaceDetector
 from Net import Net
 
 if __name__ == "__main__":
-    # face_detector = FaceDetector(filepath='cnn_model_3.pth')
     face_detector = FaceDetector(filepath='emotion_v3.pth')
     face_detector.run()
