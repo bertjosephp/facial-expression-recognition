980c9437ba3aa5851f34de2a9d8c5497132b9de9
Bert Joseph Prestoza <87596273+bertjosephp@users.noreply.github.com>
2024-06-07 11:29:36 -0700

    Update face detector to use model_checkpoint.pth

--- FaceDetector.py
+++ FaceDetector.py
@@ -7,7 +7,7 @@ from Net import Net
 
 class FaceDetector:
 
-    def __init__(self, source=0, filepath='emotion_v3.pth'):
+    def __init__(self, source=0, filepath='model_checkpoint.pth'):
         self.face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         self.video_capture = cv2.VideoCapture(source)
         self.model = Net()
